a7d3c6e0f3649ae99947b88592e27795580015b5
Mihyich <michail.zevahin@gmail.com>
2024-08-08 09:55:13 +0300

    Обновлен класс стандартного окна

--- WINAPI/inc/winapi_window.h
+++ WINAPI/inc/winapi_window.h
@@ -1,14 +1,43 @@
 #ifndef WINAPI_WINDOW_H
 #define WINAPI_WINDOW_H
 
+#define WIN32_LEAN_AND_MEAN 1 // Урезанный windows.h
+
 #include <Windows.h>
+#include <string>
+
+#define WINDOW_NOT_CREATED -1 // Значение флага window_is_created при не созданном окне
+#define WINDOW_CREATED 0      // Код возврата при успешном выполении функции генерации окна
+#define ERR_WINDOW_COPY 1     // Код возврата при дуюлирующей генерации окна
+#define ERR_WNDCLASS_COPY 2   // Код возврата при дублирующей регистрации класса стилей окна
+#define ERR_WNDCLASS_GEN 3    // Код возврата при неудачной регистрации класса стилей окна
+#define ERR_ADJWNDRECT 4      // Код возврата при неудачном выполнении функции AdjustWindowRectEx
+#define ERR_WINDOW_GEN 5      // Код возврата при неудачном созданиие окна
+#define ERR_WNDSHOW 6         // Код возврата при неудачном выполнении функции ShowWindow
+
+#define MAINLOOP_SUCCESS_STOP 0 // Код возврата при успешной остановке бесконечного цикла обработки событий окна
+#define MAINLOOP_NOT_STARTED 1  // Код возрата при неудачной попытке запустить бесконечный цилк обработки окна
+
+#define SUCCESS_WNDDESTROY 0 // Код возврата при удачном уничтожении окна
+#define ERR_WNDDESTROY 1     // Код возврата при неудочной попытке уничтожить окно
+#define ERR_UNREG_CLASS 2    // Код возврата при неудачной попытке удалить зарегистрированный класс окна
+#define ERR_ACCELDESTROY 3   // Код возврата при неудочной попытки удалить таблицу акселераторов
 
 class WinApiWindow
 {
-public:
-    HWND hWnd = NULL; // Дескриптор окна (ручка для обращения к окну)
+protected:
+	std::wstring ClassName;     // Имя класса окна
 	WNDCLASSEX wcx; // Класс окна (описывает как выглядит окно)
 
+	HINSTANCE hInstance = NULL;
+
+	HWND hWnd = NULL; // Дескриптор окна (ручка для обращения к окну)
+	HACCEL hAccel = NULL;       // Акселеряторы (комбинации клавиш)
+
+	DWORD error = ERROR_SUCCESS; // Переменная для получения кода о последней случившейся ошибке
+	RECT rect = {0, 0, 0, 0}; // Координаты и габариты окна
+
+public:
     /**
      * windowName - Имя окна (в заголовке)
      * hInst - дескриптор приложения
@@ -41,12 +70,29 @@ public:
 
     VOID Destroy();
 
-protected:
+	HWND getHwnd();
 
-    // возвращает rect с такими значенимяи, при которых окно имеет
-	// заданные длину и ширину, при этом оно будет расположено по центру
-	// экрана
-	RECT GetAlignRect(INT width, INT height);
+	INT getWidth();
+
+	INT getHeight();
+
+	DWORD get_last_error();
+
+protected:
+	VOID getAlignRect(RECT &dist_rect, INT width, INT height);
+
+	BOOL CreateWndProcess(
+		LPCTSTR windowName,
+		HINSTANCE hInst,
+		INT cmdShow,
+		LRESULT(WINAPI *pWndProc)(HWND, UINT, WPARAM, LPARAM),
+		LPCWSTR lpszClassName,
+		LPCWSTR lpszMenuName,
+		INT width, INT height,
+		UINT classStyle,
+		DWORD windowStyle,
+		DWORD windowStyleEx,
+		HWND hParent);
 
     BOOL RegWndClass(
 		UINT cbSize,
@@ -55,8 +101,12 @@ protected:
 		HINSTANCE hInstance,
 		HICON hIcon,
 		HCURSOR hCursor,
+		HBRUSH hbrBackground,
 		LPCWSTR lpszMenuName,
-		LPCWSTR lpszClassName);
+		LPCWSTR lpszClassName,
+		HICON hIconSm);
+
+	VOID reset_and_free_data();
 };
 
 #endif // WINDOW_H
--- WINAPI/src/winapi_window.cpp
+++ WINAPI/src/winapi_window.cpp
@@ -13,50 +13,19 @@ BOOL WinApiWindow::Create(
     DWORD windowStyleEx,
     HWND hParent)
 {
-    BOOL result = EXIT_SUCCESS;
-
-    // регистрация класса окна
-    if (!RegWndClass(sizeof(wcx), classStyle,
-		pWndProc,
-		hInst,
-		LoadIcon(NULL, IDI_APPLICATION),
-		LoadCursor(NULL, IDC_ARROW),
-		lpszMenuName,
-		lpszClassName))
-	{
-		MessageBox(NULL,
-			L"ErrorWndClassRg", L"ErrorWndTitle",
-			MB_OK | MB_ICONERROR);
-
-		result = EXIT_FAILURE;
-	}
-    else
-    {
-        hWnd = CreateWindowEx(
-            windowStyleEx, lpszClassName,
-            windowName, windowStyle,
-            0, 0,
-            width, height,
-            hParent,
-            HMENU(NULL),
-            hInst,
-            NULL);
-
-        if (!hWnd)
-        {
-            MessageBox(NULL,
-			L"ErrorWndCreate", L"ErrorWndTitle",
-			MB_OK | MB_ICONERROR);
-
-            result = EXIT_FAILURE;
-        }
-        else
-        {
-            ShowWindow(hWnd, cmdShow);
-        }
-    }
-
-    return result;
+    return CreateWndProcess(
+        windowName,
+        hInst,
+        cmdShow,
+        pWndProc,
+        lpszClassName,
+        lpszMenuName,
+        width,  height,
+        classStyle,
+        windowStyle,
+        windowStyleEx,
+        hParent
+    );
 }
 
 INT WinApiWindow::MainLoop()
@@ -99,6 +68,121 @@ VOID WinApiWindow::Destroy()
 	UnregisterClass(wcx.lpszClassName, wcx.hInstance);
 }
 
+HWND WinApiWindow::getHwnd()
+{
+    return this->hWnd;
+}
+
+INT WinApiWindow::getWidth()
+{
+    return rect.right - rect.left;
+}
+
+INT WinApiWindow::getHeight()
+{
+    return rect.bottom - rect.top;
+}
+
+DWORD WinApiWindow::get_last_error()
+{
+    return this->error;
+}
+
+VOID WinApiWindow::getAlignRect(RECT &dist_rect, INT width, INT height)
+{
+    dist_rect.left = (GetSystemMetrics(SM_CXSCREEN) - width) / 2;
+    dist_rect.right = dist_rect.left + width;
+    dist_rect.top = (GetSystemMetrics(SM_CYSCREEN) - height) / 2;
+    dist_rect.bottom = dist_rect.top + height;
+}
+
+BOOL WinApiWindow::CreateWndProcess(
+		LPCTSTR windowName,
+		HINSTANCE hInst,
+		INT cmdShow,
+		LRESULT(WINAPI *pWndProc)(HWND, UINT, WPARAM, LPARAM),
+		LPCWSTR lpszClassName,
+		LPCWSTR lpszMenuName,
+		INT width, INT height,
+		UINT classStyle,
+		DWORD windowStyle,
+		DWORD windowStyleEx,
+		HWND hParent)
+{
+    BOOL result = WINDOW_CREATED;
+
+    // регистрация класса окна
+    if (!RegWndClass(sizeof(wcx), classStyle,
+		pWndProc,
+		hInst,
+		LoadIcon(NULL, IDI_APPLICATION),
+		LoadCursor(NULL, IDC_ARROW),
+        (HBRUSH)(COLOR_WINDOW),
+		lpszMenuName,
+		lpszClassName,
+        LoadIcon(NULL, IDI_APPLICATION)))
+	{
+        reset_and_free_data();
+        error = GetLastError();
+
+		MessageBox(NULL,
+			L"ErrorWndClassRg", L"ErrorWndTitle",
+			MB_OK | MB_ICONERROR);
+
+		result = ERR_WNDCLASS_GEN;
+	}
+    else
+    {
+        this->ClassName.assign(lpszClassName);
+        this->hInstance = hInst;
+
+        getAlignRect(rect, width, height);
+
+        hWnd = CreateWindowEx(
+            windowStyleEx, lpszClassName,
+            windowName, windowStyle,
+            rect.left, rect.top,
+            width, height,
+            hParent,
+            HMENU(NULL),
+            hInst,
+            NULL);
+
+        if (!hWnd)
+        {
+            reset_and_free_data();
+            error = GetLastError();
+
+            MessageBox(NULL,
+			L"ErrorWndCreate", L"ErrorWndTitle",
+			MB_OK | MB_ICONERROR);
+
+            result = ERR_WINDOW_GEN;
+        }
+        else
+        {
+            ShowWindow(hWnd, cmdShow);
+
+            if (GetLastError() != ERROR_SUCCESS)
+            {
+                reset_and_free_data();
+                error = GetLastError();
+                result = ERR_WNDSHOW;
+            }
+            else
+            {
+                /*
+                переустановить курсор (чтобы при создании окна,
+                мышь сразу была правильной)
+                */
+                SetCursor(LoadCursor(nullptr, IDC_ARROW));
+            }
+        }
+    }
+
+    return result;
+}
+
 BOOL WinApiWindow::RegWndClass(
     UINT cbSize,
 	UINT style,
@@ -106,9 +190,13 @@ BOOL WinApiWindow::RegWndClass(
 	HINSTANCE hInstance,
 	HICON hIcon,
 	HCURSOR hCursor,
+    HBRUSH hbrBackground,
 	LPCWSTR lpszMenuName,
-	LPCWSTR lpszClassName)
+	LPCWSTR lpszClassName,
+    HICON hIconSm)
 {
+    ZeroMemory(&wcx, sizeof(WNDCLASSEX));
+
     wcx.cbSize = cbSize;
 	wcx.style = style;
 	wcx.lpfnWndProc = (WNDPROC)pWndProc;
@@ -117,9 +205,24 @@ BOOL WinApiWindow::RegWndClass(
 	wcx.hInstance = hInstance;
 	wcx.hIcon = hIcon;
 	wcx.hCursor = hCursor;
-	wcx.hbrBackground = (HBRUSH)(COLOR_WINDOW);
+    wcx.hbrBackground = hbrBackground;
 	wcx.lpszMenuName = lpszMenuName;
 	wcx.lpszClassName = lpszClassName;
-	wcx.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
-	return !RegisterClassEx(&wcx) ? FALSE : TRUE;
+    wcx.hIconSm = hIconSm;
+
+	return RegisterClassEx(&wcx);
+}
+
+VOID WinApiWindow::reset_and_free_data()
+{
+    // почистить ресурсы окна (почистить все на всякий случай)
+    DestroyWindow(this->hWnd);
+    UnregisterClass(this->ClassName.c_str(), this->hInstance);
+    DestroyAcceleratorTable(this->hAccel);
+
+    // обнулить все данные
+    this->hInstance = NULL;
+    this->hWnd = NULL;
+    this->ClassName.clear();
+    this->hAccel = NULL;
 }
