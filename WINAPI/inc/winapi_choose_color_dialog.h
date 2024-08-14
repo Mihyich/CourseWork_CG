29a109930c35186546af9f4b47f8f459f53c8755
Mihyich <michail.zevahin@gmail.com>
2024-08-14 15:34:00 +0300

    Удалены ненужные файлы библиотеки WinApi

--- WINAPI/inc/winapi_choose_color_dialog.h
+++
@@ -1,8 +0,0 @@
-#ifndef WINAPI_CHOOSE_COLOR_DIALOG_H
-#define WINAPI_CHOOSE_COLOR_DIALOG_H
-
-#include <windows.h>
-
-bool choose_color_dialog(HWND hwnd, COLORREF *rs);
-
-#endif // WINAPI_CHOOSE_COLOR_DIALOG_H
--- WINAPI/inc/winapi_pen_common.h
+++
@@ -1,8 +0,0 @@
-#ifndef WINAPI_PEN_COMMON_H
-#define WINAPI_PEN_COMMON_H
-
-#include <windows.h>
-
-COLORREF GetPenColorFromDC(HDC hDC);
-
-#endif // WINAPI_PEN_COMMON_H
