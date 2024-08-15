1e1749e49185e966eceee3fecdca5ba65a206e31
Mihyich <michail.zevahin@gmail.com>
2024-08-15 19:06:41 +0300

    Подготовка к зименения ширины окна toolbar

--- app/src/toolbar_wnd_proc.cpp
+++ app/src/toolbar_wnd_proc.cpp
@@ -2,11 +2,72 @@
     
 LRESULT CALLBACK ToolbarWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+    static bool isResizing = false;
+    static RECT resizeRect; // Прямоугольник, определяющий зону для захвата изменения размера
+    static int resizeBorderWidth = 30; // Ширина области для изменения размера
+    static POINT lastMousePos;
+    static HWND hResizeWnd = nullptr; // Окно, которое мы изменяем в данный момент
+
     switch(uMsg)
     {
         case WM_CREATE:
         {
             return EXIT_SUCCESS;
+        }
+
+        case WM_MOUSEMOVE:
+        {
+            if (isResizing && wParam == MK_LBUTTON)
+            {
+                POINT pt;
+                GetCursorPos(&pt);
+                ScreenToClient(hWnd, &pt);
+
+                int dx = pt.x - lastMousePos.x;
+                int dy = pt.y - lastMousePos.y;
+
+                int newWidth = resizeRect.right - resizeRect.left + dx;
+                int newHeight = resizeRect.bottom - resizeRect.top + dy;
+
+                // Убедимся, что окно не становится слишком маленьким
+                const int minWidth = 100;
+                const int minHeight = 100;
+
+                if (newWidth < minWidth) newWidth = minWidth;
+                if (newHeight < minHeight) newHeight = minHeight;
+
+                app::ToolbarWidth = newWidth;
+                lastMousePos = pt;
+
+                SendMessage(app::MainWnd.getHwnd(), WM_SIZE, 0, 0);
+            }
+            return EXIT_SUCCESS;
+        }
+
+        case WM_NCHITTEST:
+        {
+            LRESULT hit = DefWindowProc(hWnd, uMsg, wParam, lParam);
+
+            if (hit == HTCLIENT)
+            {
+                POINT pt;
+                GetCursorPos(&pt);
+                ScreenToClient(hWnd, &pt);
+                GetClientRect(hWnd, &resizeRect);
+
+                if (pt.x >= resizeRect.right - resizeBorderWidth)
+                {
+                    pt.x = resizeRect.right - resizeBorderWidth / 2;
+                    lastMousePos = pt;
+                    isResizing = true;
+                }
+                else
+                {
+                    isResizing = false;
+                }
+            }
+
+            return hit;
         }
 
         case WM_COMMAND:
