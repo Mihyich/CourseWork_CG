0e64942c91265eef0cce83760efd44d2a86a8c1a
Mihyich <michail.zevahin@gmail.com>
2024-08-17 02:50:14 +0300

    Добавлено сообщение переключения WIREFRAME

--- app/inc/user_msgs.h
+++ app/inc/user_msgs.h
@@ -7,8 +7,13 @@
 #define WM_INIT_GL_OPTIONS (WM_USER + 1)
 
 // Receiver: RenderWnd
+// Wparam: bool
+// Lparam: none
+#define WM_SET_WIREFRAME (WM_USER + 2)
+
+// Receiver: RenderWnd
 // Wparam: ShadowAlg
 // Lparam: none
-#define WM_SET_SHADOW_ALG  (WM_USER + 2)
+#define WM_SET_SHADOW_ALG  (WM_USER + 3)
 
 #endif // USER_MASGS_H
