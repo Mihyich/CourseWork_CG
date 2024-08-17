c9d49ba06f36907575d450e29f366db1a0c5498f
Mihyich <michail.zevahin@gmail.com>
2024-08-17 04:12:29 +0300

    Обновлен интерфейс (меню)

--- app/inc/resource.h
+++ app/inc/resource.h
@@ -7,17 +7,28 @@
 // основное меню
 #define IDR_MAINMENU 101
 
-#define ID_RND_CENTER 1008
-#define ID_RND_SCALE 1009
-#define ID_RND_ALL 1010
+// Вкладка "Файл"
+#define IDM_NEW_WINDOW 1001
+#define IDM_TAKE_FOTO 1002
+#define IDM_SCAN_SHADOW 1003
+#define IDM_LAUNCH_TESTING 1004
+#define IDM_EXIT 1016
 
-#define ID_FILE_AUTOR 1011
-#define ID_FILE_TASK 1012
+// Вкладка "Графика"
+#define IDB_LOAD_MODEL 1101
 
-#define ID_INFO_INPUT 1013
-#define ID_INFO_FORMAT 1014
-#define ID_INFO_COORD_SYSTEM 1015
+#define IDB_WIREFRAME_ON 1102
+#define IDB_WIREFRAME_OFF 1103
 
-#define ID_FILE_EXIT 1016
+// Вкладка "Вид"
+#define IDB_SHOW_TOOLBAR 1201
+#define IDB_HIDE_TOOLBAR 1202
+
+#define IDB_SHOW_RENDERWND 1203
+#define IDB_HIDE_RENDERWND 1204
+
+// Вкладка "О программе"
+
+#define IDM_AUTOR 1011
 
 #endif // RESOURECE_H
