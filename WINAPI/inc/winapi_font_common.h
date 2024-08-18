bb7cdafaf1d2bd40a5b04f8a162c31d40f3b7ba2
Mihyich <michail.zevahin@gmail.com>
2024-08-18 12:29:09 +0300

    Обновлены процедуры подгонки размера шрифта

--- WINAPI/inc/winapi_font_common.h
+++ WINAPI/inc/winapi_font_common.h
@@ -5,6 +5,16 @@
 #include "winapi_font_struct.h"
 #include "winapi_font.h"
 
+void measure_text_line(const WCHAR *text, HDC hDc, HFONT hFont, SIZE &rect);
+
+
+int decrease_font_height_for_fitting_width(HDC hDc, FontParam_t fpt, const WCHAR *text, int fit_width);
+
+int decrease_font_height_for_fitting_height(HDC hDc, FontParam_t fpt, const WCHAR *text, int fit_height);
+
+int decrease_font_height_for_fitting(HDC hDc, FontParam_t fpt, const WCHAR *text, int fit_width, int fit_height);
+
+
 int increase_font_height_for_fitting_width(HDC hDc, FontParam_t fpt, const WCHAR *text, int fit_width);
 
 int increase_font_height_for_fitting_height(HDC hDc, FontParam_t fpt, const WCHAR *text, int fit_height);
--- WINAPI/src/winapi_font_common.cpp
+++ WINAPI/src/winapi_font_common.cpp
@@ -1,6 +1,16 @@
 #include "winapi_font_common.h"
 
-int increase_font_height_for_fitting_width(HDC hDc, FontParam_t fpt, const WCHAR *text, int fit_width)
+void measure_text_line(const WCHAR *text, HDC hDc, HFONT hFont, SIZE &rect)
+{
+    size_t text_lenght = wcslen(text);
+    HFONT OldFont = (HFONT)SelectObject(hDc, (HFONT)hFont);
+    GetTextExtentPoint32(hDc, text, text_lenght, &rect);
+    SelectObject(hDc, (HFONT)OldFont);
+}
+
+
+
+int decrease_font_height_for_fitting_width(HDC hDc, FontParam_t fpt, const WCHAR *text, int fit_width)
 {
     if (!hDc || !fpt || !text)
         return 0;
@@ -26,7 +36,7 @@ int increase_font_height_for_fitting_width(HDC hDc, FontParam_t fpt, const WCHAR
     return fp.cHeight;
 }
 
-int increase_font_height_for_fitting_height(HDC hDc, FontParam_t fpt, const WCHAR *text, int fit_height)
+int decrease_font_height_for_fitting_height(HDC hDc, FontParam_t fpt, const WCHAR *text, int fit_height)
 {
     if (!hDc || !fpt || !text)
         return 0;
@@ -52,9 +62,69 @@ int increase_font_height_for_fitting_height(HDC hDc, FontParam_t fpt, const WCHA
     return fp.cHeight;
 }
 
+int decrease_font_height_for_fitting(HDC hDc, FontParam_t fpt, const WCHAR *text, int fit_width, int fit_height)
+{
+    int height_for_fit_width = decrease_font_height_for_fitting_width(hDc, fpt, text, fit_width);
+    int height_for_fit_height = decrease_font_height_for_fitting_height(hDc, fpt, text, fit_height);
+    return std::min(height_for_fit_width, height_for_fit_height);
+}
+
+
+int increase_font_height_for_fitting_width(HDC hDc, FontParam_t fpt, const WCHAR *text, int fit_width)
+{
+    if (!hDc || !fpt || !text)
+        return 0;
+
+    HFONT OldFont = nullptr;
+    HFONT CurFont = nullptr;
+
+    FontParam fp = *fpt; --fp.cHeight;
+    WinApiFont Font;
+    SIZE text_size;
+    const size_t text_lenght = wcslen(text);
+
+    do
+    {
+        ++fp.cHeight;
+        CurFont = Font.create_font(&fp);
+        OldFont = (HFONT)SelectObject(hDc, (HFONT)CurFont);
+        GetTextExtentPoint32(hDc, text, text_lenght, &text_size);
+        DeleteObject((HFONT)SelectObject(hDc, (HFONT)OldFont));
+
+    } while (text_size.cx <= fit_width && fp.cHeight > 0);
+
+    return fp.cHeight;
+}
+
+int increase_font_height_for_fitting_height(HDC hDc, FontParam_t fpt, const WCHAR *text, int fit_height)
+{
+    if (!hDc || !fpt || !text)
+        return 0;
+
+    HFONT OldFont = nullptr;
+    HFONT CurFont = nullptr;
+
+    FontParam fp = *fpt; --fp.cHeight;
+    WinApiFont Font;
+    SIZE text_size;
+    const size_t text_lenght = wcslen(text);
+
+    do
+    {
+        ++fp.cHeight;
+        CurFont = Font.create_font(&fp);
+        OldFont = (HFONT)SelectObject(hDc, (HFONT)CurFont);
+        GetTextExtentPoint32(hDc, text, text_lenght, &text_size);
+        DeleteObject((HFONT)SelectObject(hDc, (HFONT)OldFont));
+
+    } while (text_size.cy <= fit_height && fp.cHeight > 0);
+
+    return fp.cHeight;
+}
+
 int increase_font_height_for_fitting(HDC hDc, FontParam_t fpt, const WCHAR *text, int fit_width, int fit_height)
 {
     int height_for_fit_width = increase_font_height_for_fitting_width(hDc, fpt, text, fit_width);
     int height_for_fit_height = increase_font_height_for_fitting_height(hDc, fpt, text, fit_height);
-    return (height_for_fit_width < height_for_fit_height) ? height_for_fit_width : height_for_fit_height;
+    return std::min(height_for_fit_width, height_for_fit_height);
 }
