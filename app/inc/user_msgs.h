45646286ccfa6302da5f1eecbb5bb238aa311390
Mihyich <michail.zevahin@gmail.com>
2024-08-18 12:26:18 +0300

    Добавлено пользовательское событие WM_LOAD_MODEL

--- app/inc/user_msgs.h
+++ app/inc/user_msgs.h
@@ -7,13 +7,18 @@
 #define WM_INIT_GL_OPTIONS (WM_USER + 1)
 
 // Receiver: RenderWnd
+// Wparam: WCHAR* - путь к файлу
+// Lparam: BOOL - инвертирование порядка вершин
+#define WM_LOAD_MODEL (WM_USER + 2)
+
+// Receiver: RenderWnd
 // Wparam: bool
 // Lparam: none
-#define WM_SET_WIREFRAME (WM_USER + 2)
+#define WM_SET_WIREFRAME (WM_USER + 3)
 
 // Receiver: RenderWnd
 // Wparam: ShadowAlg
 // Lparam: none
-#define WM_SET_SHADOW_ALG  (WM_USER + 3)
+#define WM_SET_SHADOW_ALG  (WM_USER + 4)
 
 #endif // USER_MASGS_H
