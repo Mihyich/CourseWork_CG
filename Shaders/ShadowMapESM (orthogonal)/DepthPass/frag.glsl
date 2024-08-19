2fa129f7e071ff3b00dce16383e9a167a9a801a7
Mihyich <michail.zevahin@gmail.com>
2024-08-19 14:05:09 +0300

    Набор шейдеров для ESM orthogonal

---
+++ Shaders/ShadowMapESM (orthogonal)/DepthPass/frag.glsl
@@ -0,0 +1,11 @@
+#version 460 core
+
+layout(location = 0) out float FragDepth;
+
+uniform float expK = 0.5;
+
+void main()
+{
+    float depth = gl_FragCoord.z;
+    FragDepth = exp(expK * depth);
+}
---
+++ Shaders/ShadowMapESM (orthogonal)/RenderPass/frag.glsl
@@ -0,0 +1,40 @@
+#version 460 core
+
+out vec4 FragColor;
+
+in vec4 FragPos;
+in vec3 Normal;
+in vec4 FragPosLightSpace;
+
+uniform sampler2D shadowMap;
+uniform vec3 lightPos;
+uniform float expK = 0.5;
+
+float ShadowCalculation(vec4 fragPosLightSpace)
+{
+    vec3 projCoords = fragPosLightSpace.xyz;
+    projCoords.xy = projCoords.xy * 0.5 + 0.5;
+
+    // Глубина текущего фрагмента из карты теней
+    float closestDepth = texture(shadowMap, projCoords.xy).r; 
+    float currentDepth = exp(expK * projCoords.z);
+
+    // Проверка на тень
+    return clamp(closestDepth / currentDepth, 0.0, 1.0);
+}
+
+void main()
+{
+    vec3 color = vec3(0.5);
+    vec3 ambient = 0.15 * color;
+
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos.xyz);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * color;
+
+    float shadow = ShadowCalculation(FragPosLightSpace);                      
+    vec3 lighting = (ambient + (1.0 - shadow) * diffuse);
+
+    FragColor = vec4(lighting, 1.0);
+}
