a8596724b970460767ddcc0cb2ff1d877efdffd2
Mihyich <michail.zevahin@gmail.com>
2024-08-20 15:31:38 +0300

    Структуры для RayTracing

---
+++ app/inc/RayTraceBvhStruct.h
@@ -0,0 +1,39 @@
+#ifndef RAY_TRACE_BVH_STRUCT_H
+#define RAY_TRACE_BVH_STRUCT_H
+
+#include "Vector3D.h"
+
+// Вершинные данные
+struct RayTraceVertex
+{
+    vec3 p; // Позиция
+    vec3 n; // Нормаль
+};
+
+// Вершинные данные треугольника
+struct RayTraceTriangle
+{
+    RayTraceVertex v1; // Вершина 1
+    RayTraceVertex v2; // Вершина 2
+    RayTraceVertex v3; // Вершина 3
+};
+
+// Ограничивающая сфера
+struct RayTraceBS
+{
+    vec3 c; // центер
+    float r; // радиус
+};
+
+// Bounding Volume Hierarchy
+struct RayTraceBVHNode
+{
+    vec3 center;           // Центр Bounding Sphere
+    float radius;          // Радиус Bounding Sphere
+    int leftChild;         // Индекс левого дочернего узла в массиве
+    int rightChild;        // Индекс правого дочернего узла в массиве
+    int triangleIndex;     // Индекс первого треугольника (или -1, если это не лист)
+    int modelIndex;        // Индекс матрицы модели для текущего узла (для использования в шейдере)
+};
+
+#endif // RAY_TRACE_BVH_STRUCT_H
