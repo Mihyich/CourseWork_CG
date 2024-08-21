2f4acd062da1be621bb6656c675c07c62eaeb5cd
Mihyich <michail.zevahin@gmail.com>
2024-08-21 11:16:38 +0300

    Генерация буферов SSBO для RayTracing

---
+++ app/inc/RayTraceBufferGenerator.h
@@ -0,0 +1,17 @@
+#ifndef RAY_TRACE_BUFFER_GENERATOR_H
+#define RAY_TRACE_BUFFER_GENERATOR_H
+
+#include <vector>
+
+#include "Matrix4D.h"
+#include "RayTraceBvhStruct.h"
+
+#include "winapi_GLextensions.h"
+
+bool genRayTraceVertexSSBO(const std::vector<RayTraceVertexTringle>& verteces, GLuint& SSBO);
+
+bool genRayTraceMatrixSSBO(const std::vector<mat4>& matrices, GLuint& SSBO);
+
+bool genRayTraceBvhSSBO(const std::vector<RayTraceBVHNode>& bvh, GLuint& SSBO);
+
+#endif // RAY_TRACE_BUFFER_GENERATOR_H
