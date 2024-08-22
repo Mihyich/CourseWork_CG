c97fd08f53cd3ad3f0dd3e08c4d2e5ae2f63ccdc
Mihyich <michail.zevahin@gmail.com>
2024-08-22 07:58:03 +0300

    Для разных узлов BVH выбирается разный цвет

--- Shaders/RayTraceBvhDebug/geom.glsl
+++ Shaders/RayTraceBvhDebug/geom.glsl
@@ -81,7 +81,7 @@ uniform int endNodeIndex;
 out vec3 color;
 
 // Генерация примитива - треугольник
-void genPrimitiveTriangle(RayTraceVertexTringle triangle, mat4 model)
+void genPrimitiveTriangle(RayTraceVertexTringle triangle, mat4 model, vec3 col)
 {
     mat4 pvm = projection * view * model;
 
@@ -89,7 +89,7 @@ void genPrimitiveTriangle(RayTraceVertexTringle triangle, mat4 model)
     vec4 v2 = pvm * vec4(triangle.v2.px, triangle.v2.py, triangle.v2.pz, 1.0);
     vec4 v3 = pvm * vec4(triangle.v3.px, triangle.v3.py, triangle.v3.pz, 1.0);
 
-    color = vec3(0.0, 1.0, 0.0);
+    color = col;
 
     gl_Position = v1; EmitVertex();
     gl_Position = v2; EmitVertex();
@@ -98,7 +98,7 @@ void genPrimitiveTriangle(RayTraceVertexTringle triangle, mat4 model)
 }
 
 // Генерация Box (описанный вокруг сферы куб)
-void genPrimitiveBox(RayTraceBS RTBS, mat4 model)
+void genPrimitiveBox(RayTraceBS RTBS, mat4 model, vec3 col)
 {
     mat4 pvm = projection * view * model;
     float hs = RTBS.r; // half size стороны коробки
@@ -114,7 +114,7 @@ void genPrimitiveBox(RayTraceBS RTBS, mat4 model)
     vec4 v7 = pvm * vec4(center + vec3(-hs,  hs, -hs), 1.0); // левая верхняя ближняя
     vec4 v8 = pvm * vec4(center + vec3( hs,  hs, -hs), 1.0); // правая верхняя ближняя
 
-    color = vec3(1.0, 0.0, 0.0);
+    color = col;
 
     // правая && верхняя && левая грани
     gl_Position = v2; EmitVertex();
@@ -142,6 +142,7 @@ void genPrimitiveBox(RayTraceBS RTBS, mat4 model)
 void main()
 {
     int numBvhNodes = bvh.length();
+    vec3 col;
     RayTraceBVHNode node;
 
     // Обход элементов дерева через массив напрямую
@@ -153,13 +154,15 @@ void main()
         if (node.DI.matrix >= 0)
         {
             // Сфера - слишком много треугольников (аппаратное ограничение), поэтому обойдемся коробками
-            genPrimitiveBox(node.BS, matrices[node.DI.matrix]);
+            col = node.DI.triangle >= 0 ? vec3(1, 0, 0) : vec3(0, 0, 1);
+            genPrimitiveBox(node.BS, matrices[node.DI.matrix], col);
         }
 
         // Нужно нарисовать треугольник
         if (node.DI.triangle >= 0)
         {
-            genPrimitiveTriangle(triangles[node.DI.triangle], matrices[node.DI.matrix]);
+            col = vec3(0, 1, 0);
+            genPrimitiveTriangle(triangles[node.DI.triangle], matrices[node.DI.matrix], col);
         }
     }
 }
