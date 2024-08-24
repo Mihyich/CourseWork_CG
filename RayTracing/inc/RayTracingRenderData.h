f333b554e8584b76c4c10dd6cb598818bdd6932c
Mihyich <michail.zevahin@gmail.com>
2024-08-25 02:07:18 +0300

    Обновлен union для RayTracing

--- RayTracing/inc/RayTracingRenderData.h
+++ RayTracing/inc/RayTracingRenderData.h
@@ -3,11 +3,13 @@
 
 #include "RayTracingDebugRenderData.h"
 #include "RayTracingHardRenderData.h"
+#include "RayTracingSoftRenderData.h"
 
 union RayTracingRenderData
 {
     RayTracingDebugRenderData debug;
     RayTracingHardRenderData hard;
+    RayTracingSoftRenderData soft;
 };
 
 #endif // RAY_TRACING_HARD_RENDER_DATA_H
