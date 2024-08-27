74f1abce1ae63e5329368b390998966cec7e4958
Mihyich <michail.zevahin@gmail.com>
2024-08-27 18:07:22 +0300

    Перечислены пользовательские сообщения для изменения параметров источника света

--- app/inc/user_msgs.h
+++ app/inc/user_msgs.h
@@ -21,4 +21,45 @@
 // Lparam: none
 #define WM_SET_SHADOW_ALG  (WM_USER + 4)
 
+// Receiver: RenderWnd
+// Wparam: cpvec3
+// Lparam: none
+#define WM_SET_LIGHT_POSITION (WM_USER + 5)
+
+// Receiver: RenderWnd
+// Wparam: cpvec3
+// Lparam: none
+#define WM_SET_LIGHT_DIRECTION (WM_USER + 6)
+
+// Receiver: RenderWnd
+// Wparam: cpvec3
+// Lparam: none
+#define WM_SET_LIGHT_COLOR (WM_USER + 7)
+
+// Receiver: RenderWnd
+// Wparam: float*
+// Lparam: none
+#define WM_SET_LIGHT_RADIUS (WM_USER + 8)
+
+// Receiver: RenderWnd
+// Wparam: float*
+// Lparam: none
+#define WM_SET_LIGHT_INTENSITY (WM_USER + 9)
+
+// Receiver: RenderWnd
+// Wparam: float*
+// Lparam: none
+#define WM_SET_LIGHT_INNER_CUTOFF (WM_USER + 10)
+
+// Receiver: RenderWnd
+// Wparam: float*
+// Lparam: none
+#define WM_SET_LIGHT_OUTER_CUTOFF (WM_USER + 11)
+
+// Receiver: RenderWnd
+// Wparam: LightType
+// Lparam: none
+#define WM_SET_LIGHT_TYPE (WM_USER + 12)
+
+
 #endif // USER_MASGS_H
