f711d5af3c8ae280b356c5b03b4eceb699d146ca
Mihyich <michail.zevahin@gmail.com>
2024-08-27 23:11:43 +0300

    Форматирование для ввода (float)

---
+++ app/inc/formater.h
@@ -0,0 +1,9 @@
+#ifndef FORMATER_H
+#define FORMATER_H
+
+#include <iostream>
+#include <regex>
+
+bool convert_str_to_float(const wchar_t *str, float *value);
+
+#endif // FORMATER_H
---
+++ app/src/formater.cpp
@@ -0,0 +1,31 @@
+#include "formater.h"
+
+bool convert_str_to_float(const wchar_t *str, float *value)
+{
+    bool result = true;
+    wchar_t* endptr;
+
+    try
+    {
+        *value = std::wcstof(str, &endptr);
+
+        if (str == endptr)
+        {
+            throw std::invalid_argument("No digits were found");
+        }
+    }
+    catch (const std::invalid_argument& e)
+    {
+        // Invalid argument
+        // e.what()
+        result = false;
+    }
+    catch (const std::out_of_range& e)
+    {
+        // Out of range
+        // e.what()
+        result = false;
+    }
+
+    return result;
+}
