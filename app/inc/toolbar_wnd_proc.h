c64f4f8ab961bba289f78fa7b984b324ce10b203
Mihyich <michail.zevahin@gmail.com>
2024-08-29 19:17:34 +0300

    Почищены  макросы

--- app/inc/toolbar_wnd_proc.h
+++ app/inc/toolbar_wnd_proc.h
@@ -15,17 +15,6 @@
 
 #include "app_args.h"
 
-#define IDB_SURFACE_FUNC 1
-
-#define IDB_X_LIMIT 2
-#define IDB_Z_LIMIT 3
-
-#define IDB_X_STEP 4
-#define IDB_Z_STEP 5
-
-#define IDB_CHOOSE_SURFACE_COLOR 6
-#define IDB_SURFACE_LAUNCH 7
-
 LRESULT CALLBACK ToolbarWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 #endif // TOOLBAR_WND_PROC_H
