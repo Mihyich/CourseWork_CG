6cef6252bcc36e749eb3908ecdd0798865b02151
Mihyich <michail.zevahin@gmail.com>
2024-08-29 18:06:01 +0300

    Создано основное окно найтроек теней

---
+++ app/inc/shadow_wnd_proc.h
@@ -0,0 +1,19 @@
+#ifndef SHADOW_WND_PROC_H
+#define SHADOW_WND_PROC_H
+
+#include <Windows.h>
+#include <CommCtrl.h> // для общих элементов управления окна
+#include <string>
+
+#include "winapi_window.h"
+#include "winapi_common.h"
+#include "winapi_font_common.h"
+#include "winapi_mat_ext.h"
+
+#include "lighting_wnd_proc.h"
+
+#include "app_args.h"
+
+LRESULT CALLBACK ShadowWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+#endif // SHADOW_WND_PROC_H
---
+++ app/src/shadow_wnd_proc.cpp
@@ -0,0 +1,167 @@
+#include "shadow_wnd_proc.h"
+
+#define IDB_TAB_SHADOW_MAP_OPTION 1
+#define IDB_TAB_RAY_TRACING_OPTION 2
+#define IDB_TAB_PATH_TRACING_OPTION 3
+
+LRESULT CALLBACK ShadowWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    static FontParam fp;
+    static HFONT font = NULL;
+
+    static HWND TabShadowMapOptionHwnd = NULL;
+    static HWND TabRayTracingOptionHwnd = NULL;
+    static HWND TabPathTracingOptionHwnd = NULL;
+
+    switch(uMsg)
+    {
+        case WM_CREATE:
+        {
+            TabShadowMapOptionHwnd = CreateWindow(
+                L"BUTTON", L"Теневые карты", 
+                WS_VISIBLE | WS_CHILD | BS_PUSHLIKE | BS_CHECKBOX,
+                0, 0, 0, 0,
+                hWnd, (HMENU)IDB_TAB_SHADOW_MAP_OPTION, app::hInst, NULL
+            );
+
+            TabRayTracingOptionHwnd = CreateWindow(
+                L"BUTTON", L"Трассировка лучей", 
+                WS_VISIBLE | WS_CHILD | BS_PUSHLIKE | BS_CHECKBOX,
+                0, 0, 0, 0,
+                hWnd, (HMENU)IDB_TAB_RAY_TRACING_OPTION, app::hInst, NULL
+            );
+
+            TabPathTracingOptionHwnd = CreateWindow(
+                L"BUTTON", L"Трассировка путей", 
+                WS_VISIBLE | WS_CHILD | BS_PUSHLIKE | BS_CHECKBOX,
+                0, 0, 0, 0,
+                hWnd, (HMENU)IDB_TAB_PATH_TRACING_OPTION, app::hInst, NULL
+            );
+
+            fp.cWidth = 0;
+            fp.cHeight = 24;
+            fp.cEscapement = 0;
+            fp.cOrientation = 0;
+            fp.cWeight = FW_NORMAL;
+            fp.bItalic = FALSE;
+            fp.bUnderline = FALSE;
+            fp.bStrikeOut = FALSE;
+            fp.iCharSet = DEFAULT_CHARSET;
+            fp.iOutPrecision = OUT_OUTLINE_PRECIS;
+            fp.iClipPrecision = CLIP_DEFAULT_PRECIS;
+            fp.iQuality = CLEARTYPE_QUALITY;
+            fp.iPitchAndFamily = FIXED_PITCH;
+            fp.pszFaceName = L"Arial";
+
+            return EXIT_SUCCESS;
+        }
+
+        case WM_SIZE:
+        {
+            HDC hDc = GetDC(hWnd);
+            HFONT hFont;
+            RECT tmp_rect;
+            FontParam tmpFp;
+            int mainH = fp.cHeight;
+
+            int posX = 0;
+            int posY = 0;
+            int w = app::ToolbarWidth / 3;
+            int h = 20;
+
+            MoveWindow(
+                TabShadowMapOptionHwnd,
+                posX, posY, w, h,
+                TRUE
+            );
+
+            posX += w;
+
+            MoveWindow(
+                TabRayTracingOptionHwnd,
+                posX, posY, w, h,
+                TRUE
+            );
+
+            posX += w;
+
+            MoveWindow(
+                TabPathTracingOptionHwnd,
+                posX, posY, w, h,
+                TRUE
+            );
+
+            GetWindowRect(TabShadowMapOptionHwnd, &tmp_rect);
+            tmpFp = fp;
+            tmpFp.cHeight = decrease_font_height_for_fitting(hDc, &tmpFp, L"Трассировка путей", get_rect_width(tmp_rect), 20);
+            hFont = WinApiFont::create_font(&tmpFp);
+            if (hFont)
+            {
+                font = hFont;
+
+                SendMessage(TabShadowMapOptionHwnd, WM_SETFONT, (WPARAM)font, TRUE);
+                SendMessage(TabRayTracingOptionHwnd, WM_SETFONT, (WPARAM)font, TRUE);
+                SendMessage(TabPathTracingOptionHwnd, WM_SETFONT, (WPARAM)font, TRUE);
+            }
+
+            ReleaseDC(hWnd, hDc);
+            return EXIT_SUCCESS;
+        }
+
+        case WM_COMMAND:
+        {                
+            switch (LOWORD(wParam))
+            {
+                case IDB_TAB_SHADOW_MAP_OPTION:
+                {
+                    SetFocus(hWnd);
+
+                    SendMessage(TabShadowMapOptionHwnd, BM_SETCHECK, BST_CHECKED, 0);
+                    SendMessage(TabRayTracingOptionHwnd, BM_SETCHECK, BST_UNCHECKED, 0);
+                    SendMessage(TabPathTracingOptionHwnd, BM_SETCHECK, BST_UNCHECKED, 0);
+
+                    return EXIT_SUCCESS;
+                }
+
+                case IDB_TAB_RAY_TRACING_OPTION:
+                {
+                    SetFocus(hWnd);
+
+                    SendMessage(TabShadowMapOptionHwnd, BM_SETCHECK, BST_UNCHECKED, 0);
+                    SendMessage(TabRayTracingOptionHwnd, BM_SETCHECK, BST_CHECKED, 0);
+                    SendMessage(TabPathTracingOptionHwnd, BM_SETCHECK, BST_UNCHECKED, 0);
+
+                    return EXIT_SUCCESS;
+                }
+
+                case IDB_TAB_PATH_TRACING_OPTION:
+                {
+                    SetFocus(hWnd);
+
+                    SendMessage(TabShadowMapOptionHwnd, BM_SETCHECK, BST_UNCHECKED, 0);
+                    SendMessage(TabRayTracingOptionHwnd, BM_SETCHECK, BST_UNCHECKED, 0);
+                    SendMessage(TabPathTracingOptionHwnd, BM_SETCHECK, BST_CHECKED, 0);
+
+                    return EXIT_SUCCESS;
+                }
+
+                default:
+                    return EXIT_SUCCESS;
+            }
+        }
+
+        case WM_DESTROY:
+        {
+            HDC hDc = GetDC(hWnd);
+
+            if (font) DeleteObject((HFONT)SelectObject(hDc, (HFONT)font));
+
+            ReleaseDC(hWnd, hDc);
+
+            return DefWindowProc(hWnd, uMsg, wParam, lParam);    
+        }
+
+        default:
+            return DefWindowProc(hWnd, uMsg, wParam, lParam);
+    }
+}
