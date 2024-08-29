864a2eccd3b9c7149c160ea3037b928e1b8cf94e
Mihyich <michail.zevahin@gmail.com>
2024-08-29 19:42:03 +0300

    Убраны лишние зависимости

--- app/inc/model_wnd_proc.h
+++ app/inc/model_wnd_proc.h
@@ -10,9 +10,6 @@
 #include "winapi_font_common.h"
 #include "winapi_mat_ext.h"
 
-#include "shadow_wnd_proc.h"
-#include "lighting_wnd_proc.h"
-
 #include "app_args.h"
 
 LRESULT CALLBACK ModelWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
