df76d64863219cfe2059b16eafb1802a25e57375
GrizzlyBear <84165228+Mihyich@users.noreply.github.com>
2024-09-03 18:07:38 +0300

    Установлены максимальные значения для разрешения карты теней

---
+++ app/inc/ResolutionMapLimit.h
@@ -0,0 +1,8 @@
+#ifndef RESOLUTION_MAP_LIMIT_H
+#define RESOLUTION_MAP_LIMIT_H
+
+// Максимальное разрешение карты теней
+#define RESOLUTION_MAP_MAX_X 16384 // 2^14
+#define RESOLUTION_MAP_MAX_Y 16384 // 2^14
+
+#endif // RESOLUTION_MAP_LIMIT_H
