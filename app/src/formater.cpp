2d58af384d42141ba271e69f3fb8ea263ace700f
GrizzlyBear <84165228+Mihyich@users.noreply.github.com>
2024-09-03 18:28:12 +0300

    Реализация перевода строки WCHAR в LONG

--- app/src/formater.cpp
+++ app/src/formater.cpp
@@ -29,3 +29,33 @@ bool convert_str_to_float(const wchar_t *str, float *value)
 
     return result;
 }
+
+bool convert_str_to_int(const wchar_t *str, long *value)
+{
+    bool result = true;
+    wchar_t* endptr;
+
+    try
+    {
+        *value = std::wcstol(str, &endptr, 10);
+
+        if (str == endptr)
+        {
+            throw std::invalid_argument("No digits were found");
+        }
+    }
+    catch (const std::invalid_argument& e)
+    {
+        // Invalid argument
+        // e.what()
+        result = false;
+    }
+    catch (const std::out_of_range& e)
+    {
+        // Out of range
+        // e.what()
+        result = false;
+    }
+
+    return result;
+}
