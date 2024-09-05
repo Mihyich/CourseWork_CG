3ba7363ac400d99902b93c85db461c7c44d4d44a
GrizzlyBear <84165228+Mihyich@users.noreply.github.com>
2024-09-05 17:56:00 +0300

    Реализация ShadowMap Noise

---
+++ ShadowMap/inc/ShadowMapNoise.h
@@ -0,0 +1,10 @@
+#ifndef SHADOW_MAP_NOISE_H
+#define SHADOW_MAP_NOISE_H
+
+#include "ShadowMapNoiseRenderData.h"
+
+void ShadowMapNoise(ShadowMapNoiseRenderData& data);
+
+void ShadowMapNoiseDebug(ShadowMapNoiseRenderData& data);
+
+#endif // SHADOW_MAP_NOISE_H
