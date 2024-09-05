de0c656d76aaa235a7bf2b509ba47284c1621102
GrizzlyBear <84165228+Mihyich@users.noreply.github.com>
2024-09-05 20:57:28 +0300

    Комплект шейдеров для ShadowMap Noise Orthogonal

---
+++ Shaders/ShadowMapNoise (orthogonal)/RenderPass/vert.glsl
@@ -0,0 +1,23 @@
+#version 460 core
+
+layout(location = 0) in vec3 aPos;
+layout(location = 1) in vec3 aNormal;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+uniform mat4 lightView;
+uniform mat4 lightProjection;
+
+out vec4 FragPos;
+out vec3 Normal;
+out vec4 FragPosLightSpace;
+
+void main()
+{
+    FragPos = model * vec4(aPos, 1.0);
+    Normal = mat3(transpose(inverse(model))) * aNormal;
+    FragPosLightSpace = lightProjection * lightView * FragPos;
+    gl_Position = projection * view * FragPos;
+}
