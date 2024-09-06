dd8e1e3dc1fb22b9a44e9dc52cacd1bf85eff1ff
GrizzlyBear <84165228+Mihyich@users.noreply.github.com>
2024-09-06 09:04:42 +0300

    Реализация ShadowMapPcss

---
+++ ShadowMap/inc/ShadowMapPcss.h
@@ -0,0 +1,10 @@
+#ifndef SHADOW_MAP_PCSS_H
+#define SHADOW_MAP_PCSS_H
+
+#include "ShadowMapPcssRenderData.h"
+
+void ShadowMapPcss(ShadowMapPcssRenderData& data);
+
+void ShadowMapPcssDebug(ShadowMapPcssRenderData& data);
+
+#endif // SHADOW_MAP_PCSS_H
